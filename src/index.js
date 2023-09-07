8c7b4a92da2deb64901161bacefd21736b12fffc
Diana Verevkina <diana_verevkina@mail.ru>
2023-09-07 21:20:06 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './css/table.css';
+import './js/showTable';
---
+++ src/js/showTable.js
@@ -0,0 +1,5 @@
+import Table from './table';
+
+const tableContainer = document.querySelector('.table');
+const table = new Table(tableContainer);
+table.drawTable();
